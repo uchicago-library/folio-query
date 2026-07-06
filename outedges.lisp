2e7b8a000c774a6bc912b811231ac63a571ea5ea
desin@uchicago.edu <desin@uchicago.edu>
2026-07-06 16:05:11 -0500

    Schema endpoint parser added

---
+++ outedges.lisp
@@ -0,0 +1,21 @@
+(load #P"~/quicklisp/setup.lisp")
+(ql:quickload '#:com.inuoe.jzon)
+(sb-ext:add-package-local-nickname '#:jzon '#:com.inuoe.jzon)
+
+(defun parse-edges (file-name)
+  (let ((job (jzon:parse (merge-pathnames file-name *default-pathname-defaults*))) (acc '()))
+    (labels ((walk (node)
+	       (cond
+		 ((hash-table-p node)
+		  (maphash (lambda (key value)
+			     (cond
+			       ((string= "folio:linkBase" key) (push (list value) acc))
+			       ((string= "folio:linkFromField" key) (push value (first acc)))
+				 (t (walk value))))
+			   node))
+		 ((stringp node) nil)
+		 ((vectorp node)
+		  (loop for el across node
+			do (walk el))))))
+      (walk job))
+    acc))

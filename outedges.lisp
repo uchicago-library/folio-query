1f8c940b5801041ba1f9ba03021d17099668056a
desin0902 <desin@uchicago.edu>
2026-07-22 20:05:17 -0500

    Minor addition

--- outedges.lisp
+++ outedges.lisp
@@ -7,41 +7,41 @@
 (defun parse-edges (file-name)
   (let ((job (jzon:parse (merge-pathnames file-name *default-pathname-defaults*))) (acc '()))
     (labels ((walk (node)
-	       (cond
-		 ((hash-table-p node)
-		  (maphash (lambda (key value)
-			     (cond
-			       ((string= "folio:linkBase" key) (push (list (concatenate 'string "/" value)) acc))
-			       ((string= "folio:linkFromField" key) (push  value (first acc)))
-				 (t (walk value))))
-			   node))
-		 ((stringp node) nil)
-		 ((vectorp node)
-		  (loop for el across node
-			do (walk el))))))
+                   (cond
+                    ((hash-table-p node)
+                      (maphash (lambda (key value)
+                                 (cond
+                                  ((string= "folio:linkBase" key) (push (list (concatenate 'string "/" value)) acc))
+                                  ((string= "folio:linkFromField" key) (push value (first acc)))
+                                  (t (walk value))))
+                               node))
+                    ((stringp node) nil)
+                    ((vectorp node)
+                      (loop for el across node
+                            do (walk el))))))
       (walk job))
     acc))
 
 (defun make-hash-edges (pairs)
   (let ((ht (make-hash-table :test #'equal)))
     (loop for pair in pairs
-	  for source = (first pair)
-	  for dest = (second pair)
-	  do (setf (gethash source ht) dest))
+          for source = (first pair)
+          for dest = (second pair)
+          do (setf (gethash source ht) dest))
     ht))
 
 (defun parse-all-endpoints (dir)
   (loop for path in (directory (merge-pathnames "**/*.json" dir))
-	append (parse-edges  path)))
+          append (parse-edges path)))
 
 (defun extract-types (raml-hash)
   (let ((types (gethash "types" raml-hash))
         (ht (make-hash-table :test #'equal)))
     (when (hash-table-p types)
-      (maphash (lambda (key value)
-                 (when (stringp value)
-                   (setf (gethash key ht) value)))
-               types))
+          (maphash (lambda (key value)
+                     (when (stringp value)
+                           (setf (gethash key ht) value)))
+                   types))
     ht))
 
 (defun starts-with (prefix str)
@@ -51,29 +51,29 @@
 (defun extract-endpoints (raml-hash types-map)
   (let ((acc ()))
     (labels ((walk (node current-path)
-               (when (hash-table-p node)
-                 (maphash (lambda (key value)
-                            (when (stringp key)
-                              (cond
-                                ((starts-with "/" key) 
-                                (if (find #\{ key)
-                                    nil
-                                    (walk value (concatenate 'string current-path key))))
-                                ((equal key "type")
-                                 (when (hash-table-p value)
-                                   (maphash (lambda (rt-key rt-value)
-                                              (declare (ignore rt-key))
-                                              (when (hash-table-p rt-value)
-                                                (let ((schema-name (or (gethash "schemaItem" rt-value)
-                                                                       (gethash "schema" rt-value))))
-                                                  (when schema-name
-                                                    (let ((schema-file (gethash schema-name types-map)))
-                                                      (when schema-file
-                                                        (push (list current-path schema-file) acc)))))))
-                                            value)))
-                                ((hash-table-p value)
-                                 (walk value current-path)))))
-                          node))))
+                   (when (hash-table-p node)
+                         (maphash (lambda (key value)
+                                    (when (stringp key)
+                                          (cond
+                                           ((starts-with "/" key)
+                                             (if (find #\{ key)
+                                                 nil
+                                                 (walk value (concatenate 'string current-path key))))
+                                           ((equal key "type")
+                                             (when (hash-table-p value)
+                                                   (maphash (lambda (rt-key rt-value)
+                                                              (declare (ignore rt-key))
+                                                              (when (hash-table-p rt-value)
+                                                                    (let ((schema-name (or (gethash "schemaItem" rt-value)
+                                                                                           (gethash "schema" rt-value))))
+                                                                      (when schema-name
+                                                                            (let ((schema-file (gethash schema-name types-map)))
+                                                                              (when schema-file
+                                                                                    (push (list current-path schema-file) acc)))))))
+                                                            value)))
+                                           ((hash-table-p value)
+                                             (walk value current-path)))))
+                                  node))))
       (walk raml-hash ""))
     acc))
 
@@ -84,10 +84,26 @@
 
 (defun parse-all-raml (dir)
   (loop for path in (directory (merge-pathnames "*.raml" dir))
-	append (parse-raml-endpoints  path)))
+          append (parse-raml-endpoints path)))
 
 (defun reverse-mapping-hash (pairs)
   (let ((ht (make-hash-table :test #'equal)))
     (loop for (endpoint schema) in pairs
           do (setf (gethash schema ht) endpoint))
+    ht))
+
+(defun parse-arrow-line (line)
+  (let ((pos (search " -> " line)))
+    (when pos
+          (values (subseq line 0 pos)
+            (subseq line (+ pos 4))))))
+
+(defun database-endpoint-nickname (file-path)
+  (let ((ht (make-hash-table :test #'equal)))
+    (with-open-file (stream file-path)
+      (loop for line = (read-line stream nil)
+            while line
+            do (multiple-value-bind (dbname endpoint) (parse-arrow-line line)
+                 (when dbname
+                       (setf (gethash dbname ht) endpoint)))))
     ht))

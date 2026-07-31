82fe1179f02935dcad6341b331736078c870eeed
desin@uchicago.edu <desin@uchicago.edu>
2026-07-31 11:25:04 -0500

    New functionality added. Okapi-login and token caching, as well as login-curl prints

--- outedges.lisp
+++ outedges.lisp
@@ -4,6 +4,7 @@
   (sb-ext:add-package-local-nickname '#:jzon '#:com.inuoe.jzon)
   (ql:quickload "cl-yaml"))
 
+;; Parses all folio:linkBase and folio:linkFromField edges and returns them in ((out to) (out to)...) format. Since folio: internal graph edges are limited, this function alone cannot sufficiently populate the edge map
 (defun parse-edges (file-name)
   (let ((job (jzon:parse (merge-pathnames file-name *default-pathname-defaults*))) (acc '()))
     (labels ((walk (node)
@@ -22,6 +23,7 @@
       (walk job))
     acc))
 
+;; Hashes a list of ((out to)...) into an edge hashmap 
 (defun make-hash-edges (pairs)
   (let ((ht (make-hash-table :test #'equal)))
     (loop for pair in pairs
@@ -30,10 +32,12 @@
           do (setf (gethash source ht) dest))
     ht))
 
+;; Parses all of the folio: internal graph edges across all jsons
 (defun parse-all-endpoints (dir)
   (loop for path in (directory (merge-pathnames "**/*.json" dir))
           append (parse-edges path)))
 
+;; Extracts all of the types from a raml hash
 (defun extract-types (raml-hash)
   (let ((types (gethash "types" raml-hash))
         (ht (make-hash-table :test #'equal)))
@@ -44,10 +48,12 @@
                    types))
     ht))
 
+;; Returns t if str starts with prefix and nil o/w. (starts-with prefix str)
 (defun starts-with (prefix str)
   (and (>= (length str) (length prefix))
        (string= str prefix :end1 (length prefix))))
 
+;; Extracts all endpoints from the provided raml given the types-map
 (defun extract-endpoints (raml-hash types-map)
   (let ((acc ()))
     (labels ((walk (node current-path)
@@ -77,27 +83,32 @@
       (walk raml-hash ""))
     acc))
 
+;; Parses the provided raml's endpoints
 (defun parse-raml-endpoints (file-path)
   (let* ((raml (yaml:parse file-path))
          (types-map (extract-types raml)))
     (extract-endpoints raml types-map)))
 
+;; Parses all of the raml in the directory and stores their endpoints (in the wrong order) in a list of pairs
 (defun parse-all-raml (dir)
   (loop for path in (directory (merge-pathnames "*.raml" dir))
           append (parse-raml-endpoints path)))
 
+;; Takes a list of pairs and hashes it in reverse order such that (val key) becomes key: val in the hash
 (defun reverse-mapping-hash (pairs)
   (let ((ht (make-hash-table :test #'equal)))
     (loop for (endpoint schema) in pairs
           do (setf (gethash schema ht) endpoint))
     ht))
 
+;; Simple parsing function for how I stored my internal database name -> schema name list
 (defun parse-arrow-line (line)
   (let ((pos (search " -> " line)))
     (when pos
           (values (subseq line 0 pos)
             (subseq line (+ pos 4))))))
 
+;; Returns a hash of database nicknames to real endpoints used when parsing the schema.json key to endpoint map
 (defun database-endpoint-nickname (file-path)
   (let ((ht (make-hash-table :test #'equal)))
     (with-open-file (stream file-path)
@@ -107,3 +118,49 @@
                  (when dbname
                        (setf (gethash dbname ht) endpoint)))))
     ht))
+
+;; Parses all of the foreign-key -> endpoint mappings in schema.json, using the nickname file to map sql endpoint names to real endpoint names.
+(defun parse-db-foreign-keys (schema-file nickname-file)
+  (let* ((schema (jzon:parse (merge-pathnames schema-file *default-pathname-defaults*)))
+         (nicknames (database-endpoint-nickname nickname-file))
+         (tables (gethash "tables" schema))
+         (acc ()))
+    (loop for table across tables
+          when (hash-table-p table)
+          do (let ((foreign-keys (gethash "foreignKeys" table)))
+               (when (vectorp foreign-keys)
+                     (loop for fk across foreign-keys
+                           when (hash-table-p fk)
+                           do (let ((field-name (gethash "fieldName" fk))
+                                    (target-table (gethash "targetTable" fk)))
+                                (when (and field-name target-table)
+                                      (let ((endpoint (gethash target-table nicknames)))
+                                        (when endpoint
+                                              (pushnew (list field-name endpoint)
+                                                       acc
+                                                       :test #'equal)))))))))
+    acc))
+
+;; A function to be run once that rewrote my sql internal name to endpoint map file to one that uses the full endpoint name rather than the partial one that I wrote.
+(defun write-full-path-nicknames (nickname-file raml-dir output-file)
+  (let ((nicknames (database-endpoint-nickname (merge-pathnames nickname-file *default-pathname-defaults*)))
+         (raml-pairs (parse-all-raml raml-dir))
+         (filename-to-full-path (make-hash-table :test #'equal)))
+    (loop for (endpoint schema-path) in raml-pairs
+          for bare = (file-namestring (pathname schema-path))
+          do (setf (gethash bare filename-to-full-path) schema-path))
+    (with-open-file (out output-file :direction :output :if-exists :supersede)
+      (maphash (lambda (table-name bare-json)
+                 (let ((full-path (gethash bare-json filename-to-full-path)))
+                   (when full-path
+                         (format out "~A -> ~A~%" table-name full-path))))
+               nicknames))))
+
+;; Maps ids to the target endpoint 
+(defun id-to-endpoint-db (raml-dir schema-file nickname-file)
+  (let ((schema-to-endpoint (reverse-mapping-hash (parse-all-raml raml-dir)))
+	(fk-pairs (parse-db-foreign-keys schema-file nickname-file)))
+    (loop for (field-name json-file) in fk-pairs
+	  for endpoint = (gethash json-file schema-to-endpoint)
+          when endpoint
+            collect (list field-name endpoint))))
--- query.lisp
+++ query.lisp
@@ -4,6 +4,7 @@
   (sb-ext:add-package-local-nickname '#:jzon '#:com.inuoe.jzon)
   (load #P"outedges.lisp"))
 
+;; Simple test function for I/O. Liable to be deleted
 (defun my-echo ()
 (loop
   for line = (read-line nil nil)
@@ -14,6 +15,7 @@
   do (write-line "preface")
   do (write-line line)))
 
+;; Used for REPL json input. No longer used, now that graphs are build automatically
 (defun parse-cli-json ()
   (let ((input (with-output-to-string (s)
                  (loop for line = (read-line nil nil)
@@ -21,20 +23,24 @@
                        do (write-string line s)))))
     (jzon:parse input)))
 
+;; Prints all nodes in an adj-list to stdout in a "NodeA" format
 (defun list-nodes (adj-list)
   (loop for node in adj-list
 	do (write-line (format nil "~A" (first node)))))
 
+;; Prints all edges in an adj-list to stdout in "NodeA to NodeB" format
 (defun list-edges (adj-list)
   (loop for node in adj-list
 	for cur = (first node)
 	do (loop for next in (cadr node)
 		 do (write-line (format nil "~A to ~A" cur next)))))
 
+;; Basic queue struct
 (defstruct (queue (:constructor make-queue ()))
   (head nil :type list)
   (tail nil :type list))
 
+;; Enqueues an item into the queue. (enqueue item queue)
 (defun enqueue (item queue)
   (let ((cell (list item)))
     (if (queue-tail queue)
@@ -42,6 +48,7 @@
         (setf (queue-head queue) cell))
     (setf (queue-tail queue) cell)))
 
+;; Dequeues an item from the queue and returns it
 (defun dequeue (queue)
   (let ((cell (queue-head queue)))
     (when cell
@@ -50,11 +57,13 @@
         (setf (queue-tail queue) nil))
       (car cell))))
 
+;; Returns the nodes adjacent to val. (adj-of adj-list val). Maybe swap param order?
 (defun adj-of (adj-list val)
   (loop for node in adj-list
 	when (equalp (first node) val)
 	  return (cadr node)))
 
+;; Backtraces the path from start to dest, or returns nil if none. (backtrace-path parent-list start dest)
 (defun backtrace-path (parent-list start dest)
   (let ((curr dest)
 	(path ()))
@@ -68,7 +77,7 @@
 	nil
 	path)))
 	
-
+;; BFS shortest path for an adj-list. 
 (defun get-shortest-path (adj-list start dest)
   (let ((visited ())
 	(parent (make-hash-table :test #'equal))
@@ -85,6 +94,7 @@
 		     and do (setf (gethash next parent) curr)))
     (backtrace-path parent start dest)))
 
+;; Returns a list of reachable nodes from start in the given adj-list (reachable adj-list start)
 (defun reachable (adj-list start)
   (let ((seen ())
 	(visited ())
@@ -101,6 +111,7 @@
 		     and do (enqueue next queue)))
     seen))
 
+;; Test function for binding anonymous functions to tree traversal. Idea was excessive for final version. Defunct.
 (defun node-func-binds ()
   (let ((binds (make-hash-table)))
     (setf (gethash 'A binds) (lambda () (format t "Node A~%")))
@@ -111,11 +122,13 @@
     (setf (gethash 'F binds) (lambda () (format t "Node F~%")))
     binds))
 
+;; Prints the provided path. Used for adj-list. Defunct.
 (defun print-path (path)
   (let ((nf-binds (node-func-binds)))
     (loop for node in path
 	  do (funcall (gethash node nf-binds)))))
 
+;; Returns the terminal values in a jobject (parsed by jzon). Largely defunct
 (defun jobject-leaves (job)
   (let ((acc ()))
     (labels ((walk (node)
@@ -133,6 +146,7 @@
       (walk job))
     acc))
 
+;; Fetches the value from a category in a jobject. Potentially useful if program does curls for user
 (defun fetch-from-jobject (category job)
   (block desired-val
     (labels ((walk (target node)
@@ -149,6 +163,7 @@
                         do (walk target el))))))
       (walk category job))))
 
+;; Returns all categories of the provided jobject
 (defun jobject-categories (job)
   (let ((acc ()))
     (labels ((walk (node)
@@ -165,18 +180,23 @@
       (walk job))
     acc))
 
+;; I don't really remember what this does. Remind me to test it later
 (defun sanitize-jobject-fetch (fetched-val)
   (if (hash-table-p fetched-val)
       (jzon:stringify fetched-val)
       fetched-val))
 
+;; A test graph used for proof of concept, and merge checks
 (defparameter *test-graph-1* '((A (B C D E*)) (B (F)) (C (G)) (D ()) (E* ()) (F (I)) (G (H)) (H (I)) (I (L)) (L ())))
 
+;; A second test graph meant to be merged into 1
 (defparameter *test-graph-2* '((E (J K*)) (J ()) (K* ())))
 
+;; A hash that defines test foreign-keys and graphs to merge
 (defparameter *graph-hash* (make-hash-table))
 (setf (gethash 'E* *graph-hash*) '(E *test-graph-2*))
 
+;; A proof-of-concept graph merge algorithm. Defunct.
 (defun graph-merge (f-graph)
   (let ((key-out *graph-hash*))
     (loop for ne-pair in f-graph
@@ -187,11 +207,13 @@
 	    and do (nconc f-graph (rest (symbol-value (cadr key-out-val))))))
   f-graph)
 
+;; Adds a node with no edges to the adj-list
 (defun add-node (node adj-list)
   (if (assoc node adj-list :test #'equal)
       adj-list
       (cons (list node ()) adj-list)))
 
+;; Add an edge from the first input to the second input in the provided adj-list
 (defun add-edge (from to adj-list)
   (loop for ne-pair in adj-list
 	for node = (first ne-pair)
@@ -199,6 +221,7 @@
 	  do (push to (second ne-pair)))
   adj-list)
 
+;; Takes a parsed jobject and returns an adj-list
 (defun json-to-adj-list (job)
   (let ((acc ()))
     (labels ((walk (parent jo)
@@ -217,6 +240,7 @@
       (walk nil job))
     acc))
 
+;; Returns true if the provided string is a uuid, and false if not. Defunct.
 (defun uuid-p (str)
   (and (stringp str)
        (= (length str) 36)
@@ -229,15 +253,19 @@
                     (char= letter #\-)))
               str)))
 
+;; Basic stack struct
 (defstruct (stack (:constructor make-stack ()))
   (top nil :type list))
 
+;; Pushes the given item to the stack
 (defun push-stack (item stack)
   (push item (stack-top stack)))
 
+;; Pops the top item from the stack and returns it
 (defun pop-stack (stack)
   (pop (stack-top stack)))
 
+;; Returns the top item on the stack without popping it
 (defun peek-stack (stack)
   (car (stack-top stack)))
 
@@ -275,17 +303,19 @@
       (walk root-name job))
     acc))
 
+;; Returns t if the str ends with the provided suffix and nil otherwise. (ends-with suffix str)
 (defun ends-with (suffix str)
   (and (>= (length str) (length suffix))
        (string= str suffix :start1 (- (length str) (length suffix)))))
 
+;; Returns str.split(char)[-1].
 (defun after-char (char str)
   (let ((pos (position char str :from-end t)))
     (if pos
 	(subseq str (1+ pos))
 	str)))
 
-;; hacky workaround just used for testing
+;; hacky workaround just used for testing (apparently a real part of the program now. Remind me to just build this into my functions so I don't have to do two passes)
 (defun prune-tag (adj-list)
   (loop for ne-pair in adj-list
 	when (ends-with "Id" (first ne-pair))
@@ -295,10 +325,12 @@
 		   do (setf (car item) (concatenate 'string (after-char #\. (car item)) (string #\*)))))
   adj-list)
 
+;; Defunct hash used for testing
 (defparameter *json-test-hash* (make-hash-table :test #'equal))
 (setf (gethash "holdingsRecordId*" *json-test-hash*) '("holdings-endpoint" *test-json-3*))
 (setf (gethash "instanceId*" *json-test-hash*) '("instances-endpoint" *test-json-1*))
 
+;; Test function used for merging json graphs. Defunct
 (defun json-graph-merge (f-graph)
   (let ((key-out *json-test-hash*))
     (loop for ne-pair in f-graph
@@ -309,6 +341,7 @@
 	    and do (nconc f-graph (rest (symbol-value (cadr key-out-val))))))
   f-graph)
 
+;; Prepends the root-name onto the base-name
 (defun prepend-endpoint (base-name root-name)
   (format nil "~A.~A" root-name base-name))
 
@@ -389,12 +422,14 @@
       (walk root-name job nil))
     acc))
 
+;; Builds a single adj-list for the given endpoint based on the schema-path
 (defun build-endpoint-graph (schema-path base-dir schema-to-endpoint-hash)
   (let ((endpoint (gethash schema-path schema-to-endpoint-hash))
         (schema (jzon:parse (pathname (merge-pathnames schema-path base-dir)))))
     (when endpoint
       (json-schema-to-adj-list schema endpoint))))
 
+;; Builds all graphs and returns a list of adj-lists
 (defun build-all-graphs (base-dir)
   (let* ((raml-pairs (parse-all-raml base-dir))
          (rev (reverse-mapping-hash raml-pairs)))
@@ -403,35 +438,56 @@
           when graph
             collect graph)))
 
+;; The default path for schema and ramls
+(defparameter *schema-and-raml-path* "mod-inventory-storage/ramls/")
+
+;; A global variable to hold all graphs in a hash table
 (defparameter *all-graphs* (make-hash-table :test #'equal))
 
-(loop for graph in (build-all-graphs "mod-inventory-storage/ramls/")
+;; Build all graphs at runtime and hash them into *all-graphs*
+(loop for graph in (build-all-graphs *schema-and-raml-path*)
       for root = (caar (last graph))
       do (setf (gethash root *all-graphs*) graph))
 
+;; override: holdingsRecordView.json 
+(setf (gethash "/holdings-storage/holdings" *all-graphs*)
+      (json-schema-to-adj-list
+       (jzon:parse (merge-pathnames "schemas/holdings-storage/holdingsRecord.json"
+                                    *schema-and-raml-path*))
+       "/holdings-storage/holdings"))
+
 ;; currently broken. Repeated id names overwrite
 (defparameter *id-to-endpoint-map* (make-hash-table :test #'equal))
-
-;; make the path a global param
-(loop for pair in (parse-all-endpoints "mod-inventory-storage/ramls/")
-      for id = (first pair)
-      for endpoint = (second pair)
+(loop for (id endpoint) in (id-to-endpoint-db *schema-and-raml-path* "schema.json" "table-to-schema-map.txt")
       do (setf (gethash id *id-to-endpoint-map*) endpoint))
 
+;; Populates *id-to-endpoint-map* at runtime
+(loop for pair in (parse-all-endpoints *schema-and-raml-path*)
+      for id = (first pair)
+      for endpoint = (second pair)
+      unless (gethash id *id-to-endpoint-map*)
+      do (setf (gethash id *id-to-endpoint-map*) endpoint))
+
+;; needed until I can disambiguate each schema's local "id" meaning
+(remhash "id" *id-to-endpoint-map*)
+
+;; Simple test-function that prints a hashtable to stdout
 (defun print-hash (ht)
   (maphash (lambda (key value)
 	     (format t "Key: ~A, Val: ~A~%" key value))
 	   ht))
 
+;; Merges the graph at id to another graph
 (defun selective-merge (id graph)
   (let* ((node (assoc id graph :test #'equal))
-         (target-graph (copy-tree (gethash (gethash (string-right-trim "*" id) *id-to-endpoint-map*) *all-graphs*)))
+         (target-graph (prune-tag (copy-tree (gethash (gethash (string-right-trim "*" id) *id-to-endpoint-map*) *all-graphs*))))
          (target-root (caar (last target-graph))))
     (when (and node target-graph)
       (setf (cadr node) (list target-root))
       (nconc graph target-graph))
     graph))
 
+;; Returns a path from start-endpoint to the target value, or the complete merged graph if no path.
 (defun merge-from-start-endpoint (start-endpoint target)
   (let ((visited ())
 	(queue (make-queue))
@@ -442,16 +498,12 @@
     (loop while (queue-head queue)
 	  for curr = (dequeue queue)
 	  for edges = (adj-of start-graph curr)
-	  do (when (string-equal curr target)
-	       (format t "MATCH: ~A~%" curr))
 	  do (cond ((string-equal curr target)
 		    (return-from merge-from-start-endpoint (backtrace-path parent start-endpoint target)))
 		   ((find #\* curr)
-		    (format t "Curr: ~A~%" curr)
 		    ;; might be tossing away an intermediate representation I want
 		    (setf start-graph (selective-merge curr start-graph))
-		    (setf edges (adj-of start-graph curr))
-		    (format t "Edges: ~A~%" edges))
+		    (setf edges (adj-of start-graph curr)))
 		   (t nil))
 	  do (loop for next in edges
 		     unless (member next visited :test #'equal)
@@ -459,6 +511,65 @@
 		       and do (enqueue next queue)
 		       and do (setf (gethash next parent) curr)))
     visited))
-	     
-	    
-  
+
+;; Simple function that checks if the provided str is an id. Separated so that it can be easily modified if I change how ids are labeled.
+(defun is-id (str)
+  (ends-with "*" str))
+
+;; Global var that tracks the base path for curls, before specific endpoints
+(defparameter *base-folio-path* "https://uchicago-test-okapi.folio.indexdata.com")
+
+;; Takes a path of form (id, endpoint, id, endpoint...) and prints it to stdout in curlable format
+(defun print-curls (path)
+  (loop for curr in path
+	and prev = nil then curr
+	when (and prev (is-id prev))
+	  do (format t "~A~A/{~A}~%" *base-folio-path* curr (string-right-trim "*" prev))))
+
+;; Pathfinds from an id to the target and prints the curls to stdout
+(defun pathfind-id (start-id target)
+  (print-curls (cons (format nil "~A~C" start-id #\*) (merge-from-start-endpoint (gethash start-id *id-to-endpoint-map*) target))))
+
+(defparameter *x-okapi-token* nil)
+(defparameter *config-file* "config.txt")
+(defparameter *okapi-username* nil)
+(defparameter *okapi-password* nil)
+
+(defun load-login-info ()
+    (with-open-file (stream *config-file*)
+      (loop for line = (read-line stream nil)
+	    while line
+	    when (search "username" line)
+	    collect (string-trim " " (after-char #\: line))
+	    when (search "password" line)
+	      collect (string-trim " " (after-char #\: line)))))
+
+(defun store-login-info (login-list)
+  (cond ((/= (length login-list) 2) (format t "Length ~A does not match (username password) structure required~%" (length login-list)))
+	(t
+	 (setf *okapi-username* (first login-list)
+	       *okapi-password* (second login-list))
+	 (values))))
+
+(defparameter *login-curl-base* nil)
+
+(defun load-login-curl ()
+  (setf *login-curl-base* "curl -w '\\n' -X POST -H \"Accept: application/json\" -H \"Content-Type: application/json\" -H \"X-Okapi-Tenant: uchicago\" https://uchicago-test-okapi.folio.indexdata.com/authn/login -d '{\"username\": \"~A\", \"password\": \"~A\"}' --include"))
+
+(load-login-curl)
+
+(defun print-login-curl ()
+  (when (or (null *okapi-username*) (null *okapi-password*))
+    (format t "Username or password is invalid~%")
+    (return-from print-login-curl nil))
+  (format t *login-curl-base* *okapi-username* *okapi-password*))
+
+
+
+;; do curl
+;; print shell command to do curls
+;; print path
+;; cache token
+;; debugging
+;; statusId
+;; make sure you don't intentionally put in security vulnerabilities
